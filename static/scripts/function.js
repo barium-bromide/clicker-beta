d6de4691588cf895c6a71ab005d007b2e9e2a05c
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2022-11-14 21:57:23 +0800

    leaderboard
    the commit message for your changes. Lines starting

--- static/scripts/function.js
+++ static/scripts/function.js
@@ -1,108 +1,31 @@
 //All comments send to database
 //apple = 0
 //claim_apple = 0
-
-document.getElementById("IncreaseApple").onclick = function IncreaseApple(){
-    //apple += 1    
-    document.getElementById("IncreaseApple").style.transform = scale(1.5);  
-    document.getElementById("countPara").innerHTML = apple;
-}
+document.getElementById("IncreaseApple").onclick = function IncreaseApple() {
+  let now = Datw.now();
+  if ((now - lastclick)/100 < 0.05){
+    alert("You are clicking too fast");
+    return;
+  }
+  let lastclick = document.getElementById("IncreaseApple").onclick;
+  //apple += 1
+  document.getElementById("IncreaseApple").style.transform = scale(1.5);
+  document.getElementById("countPara").innerHTML = apple;
+};
 
 //Price = [15, 100, 1000, 2000, 5000, 10000, 30000, 50000, 100000, 200000] => Price get expensive if buy more *1.10 everytime
 //Player_Inventory = [] => after player buy stuff from shop they got
-document.getElementById("buy0").onclick = function(){
-    if (apple < Price[0]){
-        return
+for (let i = 0; i < 10; i++) {
+  document.getElementById("buy" + i).onclick = function () {
+    if (apple < Price[i]) {
+      return;
+    } else {
+      //apple -=Price[i];
+      //Player_Inventory = [Got smtg]
+      //Price[*1.10]
+      //Change claim_apple by 1, Player must claim or gone, get elemet btn claim to autoclick thingy
+      //document.getElementById("countPara").innerHTML = apple;
+      console.log("bought item" + i);
     }
-    else{
-            //I lazy type all 10 but this same with 10 if
-            //apple -=Price[0];
-            //Player_Inventory = [Got smtg]
-            //Price[*1.10]
-            //Change claim_apple by 1, Player must claim or gone(autoclick feature shop)=> I will add the btn and claim label if i free
-            document.getElementById("countPara").innerHTML = apple;
-        }
-    }
-
-    document.getElementById("buy1").onclick = function(){
-        if (apple < Price[1]){
-            return
-        }
-        else{
-                //apple -=Price[1];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-
-    document.getElementById("buy2").onclick = function(){
-        if (apple < Price[2]){
-            return
-        }
-        else{
-                //apple -=Price[2];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy3").onclick = function(){
-        if (apple < Price[3]){
-            return
-        }
-        else{
-                //apple -=Price[3];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy4").onclick = function(){
-        if (apple < Price[4]){
-            return
-        }
-        else{
-                //apple -=Price[4];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }    
-    document.getElementById("buy5").onclick = function(){
-        if (apple < Price[5]){
-            return
-        }
-        else{
-                //apple -=Price[5];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy6").onclick = function(){
-        if (apple < Price[6]){
-            return
-        }
-        else{
-                //apple -=Price[6];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy7").onclick = function(){
-        if (apple < Price[7]){
-            return
-        }
-        else{
-                //apple -=Price[7];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy8").onclick = function(){
-        if (apple < Price[8]){
-            return
-        }
-        else{
-                //apple -=Price[8];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
-    document.getElementById("buy9").onclick = function(){
-        if (apple < Price[9]){
-            return
-        }
-        else{
-                //apple -=Price[9];
-                document.getElementById("countPara").innerHTML = apple;
-            }
-        }
+  }
+}
