fc98a1a722ee841e5e5ce6bf96ef1e26c0438712
Mortis66666 <mortisloverbs@gmail.com>
2022-11-15 15:11:12 +0800

    prettier

--- static/scripts/function.js
+++ static/scripts/function.js
@@ -2,30 +2,30 @@
 //apple = 0
 //claim_apple = 0
 document.getElementById("IncreaseApple").onclick = function IncreaseApple() {
-  let now = Datw.now();
-  if ((now - lastclick)/100 < 0.05){
-    alert("You are clicking too fast");
-    return;
+    let now = Datw.now();
+    if ((now - lastclick) / 100 < 0.05) {
+        alert("You are clicking too fast");
+        return;
     }
-  let lastclick = document.getElementById("IncreaseApple").onclick;
-  //apple += 1
-  document.getElementById("IncreaseApple").style.transform = scale(1.5);
-  document.getElementById("countPara").innerHTML = apple;
+    let lastclick = document.getElementById("IncreaseApple").onclick;
+    //apple += 1
+    document.getElementById("IncreaseApple").style.transform = scale(1.5);
+    document.getElementById("countPara").innerHTML = apple;
 };
 
 //Price = [15, 100, 1000, 2000, 5000, 10000, 30000, 50000, 100000, 200000] => Price get expensive if buy more *1.10 everytime
 //Player_Inventory = [] => after player buy stuff from shop they got
 for (let i = 0; i < 10; i++) {
-  document.getElementById("buy" + i).onclick = function () {
-    if (apple < Price[i]) {
-      return;
-    } else {
-      //apple -=Price[i];
-      //Player_Inventory = [Got smtg]
-      //Price[*1.10]
-      //Change claim_apple by 1, Player must claim or gone, get elemet btn claim to autoclick thingy
-      //document.getElementById("countPara").innerHTML = apple;
-      console.log("bought item" + i);
-    }
-  }
+    document.getElementById("buy" + i).onclick = function () {
+        if (apple < Price[i]) {
+            return;
+        } else {
+            //apple -=Price[i];
+            //Player_Inventory = [Got smtg]
+            //Price[*1.10]
+            //Change claim_apple by 1, Player must claim or gone, get elemet btn claim to autoclick thingy
+            //document.getElementById("countPara").innerHTML = apple;
+            console.log("bought item" + i);
+        }
+    };
 }
